993ee8895b6c974da3ce5951b95ac62ad7c5fd59
KarimDevTech <karim.dev.tech@gmail.com>
2022-11-17 15:56:41 -0300

    Actualizando detalles

--- Scrap bot telegram/Bot_Telegram.py
+++ Scrap bot telegram/Bot_Telegram.py
@@ -3,7 +3,9 @@ import requests
 import time
 import webbrowser
 
+#Traer la direccion de la pagina de la cual quiero la informacion
 url = requests.get("https://articulo.mercadolibre.com.ar/MLA-896435545-alpargata-clasica-reforzada-simil-yute-toro-y-pampa-_JM#backend=item_decorator&backend_type=function&client=bookmarks-polycard")
+
 
 soup = BeautifulSoup(url.content, "html.parser")
 
@@ -13,13 +15,11 @@ precioInicial = float(precioInicio_text)
 precioDeseado = 1.700
 
 
-
-
-
+#Función que se inicializa si el precio del producto esta en el rango deseado
 def telegram_bot_sendtext(bot_message):
     
     bot_token = '5653725613:AAGXOmbAGQdQar0_2k1nLYJphnysJY1-z7k'
-    bot_chatID = '5038171445'
+    bot_chatID = '0000000' #Aca va el chat ID de telegram que por motivos de privacidad no lo pongo
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
@@ -27,6 +27,6 @@ def telegram_bot_sendtext(bot_message):
     return response.json()
     
 if precioInicial <= precioDeseado:
-    test = telegram_bot_sendtext(f"HAY REBAJA, el precio esta en: {'$'+ str(precioInicial)}\nApurate K, te dejo el link\n https://articulo.mercadolibre.com.ar/MLA-896435545-alpargata-clasica-reforzada-simil-yute-toro-y-pampa-_JM#backend=item_decorator&backend_type=function&client=bookmarks-polycard")
-else: test = telegram_bot_sendtext("No hay ninguna rebaja")
+    test = telegram_bot_sendtext(f"HAY REBAJA, el precio esta en: {'$'+ str(precioInicial)}\nApurate Karim, te dejo el link\n https://articulo.mercadolibre.com.ar/MLA-896435545-alpargata-clasica-reforzada-simil-yute-toro-y-pampa-_JM#backend=item_decorator&backend_type=function&client=bookmarks-polycard")
+
 
